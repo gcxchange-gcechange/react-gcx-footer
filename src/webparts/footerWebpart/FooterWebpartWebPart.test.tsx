558cb9d7b9a3c92d6afa9b79eba65292b3f0861b
vid <vid_github@zooid.org>
2024-10-28 18:19:11 -0400

    move test alongside component, fix test with mock

--- src/webparts/footerWebpart/FooterWebpartWebPart.test.tsx
+++ src/webparts/footerWebpart/FooterWebpartWebPart.test.tsx
@@ -1,11 +1,17 @@
 ///<reference types="jest"/>
 
+jest.mock('@microsoft/sp-webpart-base', () => ({
+  WebPartContext: jest.fn().mockImplementation(() => ({
+  })),
+}));
+
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import FooterWebpart from '../components/FooterWebpart';
-import { IFooterWebpartProps } from '../components/IFooterWebpartProps'; 
 import { WebPartContext } from '@microsoft/sp-webpart-base';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+import FooterWebpart from './components/FooterWebpart';
+import { IFooterWebpartProps } from './components/IFooterWebpartProps'; 
 
 
 describe("FooterWebpart Component", () => {
@@ -21,7 +27,7 @@ describe("FooterWebpart Component", () => {
 
   it("should render FooterWebpart component", () => {
     render(<FooterWebpart {...mockProps} />);
-    expect(screen.getByText(mockProps.description)).toBeInTheDocument();
+    expect(screen.getByText(mockProps.wantToStayInTheLoopTitle)).toBeInTheDocument();
   });
 
   it("should render the 'Stay in the loop' title", () => {
