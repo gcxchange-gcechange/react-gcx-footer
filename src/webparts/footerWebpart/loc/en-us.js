c4a5190d46812cb25936923c63a858aa6d4df86e
Moreno Ramirez <gabriela.morenor@gmail.com>
2026-06-02 16:17:55 -0400

    modified footer columns and links

--- src/webparts/footerWebpart/loc/en-us.js
+++ src/webparts/footerWebpart/loc/en-us.js
@@ -4,28 +4,45 @@ define([], function() {
     "PropertyPaneDescription": "Description",
     "BasicGroupName": "Group Name",
     "DescriptionFieldLabel": "Description Field",
-    "AboutUsTitle": "THIS IS US",
-    "SiteLinksTitle": "MORE SITE SEEING",
-    "SupportLinksTitle": "REACH OUT?",
+
+    //First Column
+    "GCXchangeColumnTitle": "GCXCHANGE",
     "LinkAboutUs": "About us",
-    "LinkTerms": "GCXchange Terms and Conditions",
-    "LinkPrivacy": "Privacy policy",
-    "LinkSupport": "Support",
     "URLAboutUs": "https://gcxgce.sharepoint.com/SitePages/About-us.aspx",
+    "LinkTerms": "Terms and conditions",
     "URLTerms": "https://gcxgce.sharepoint.com/SitePages/Terms-and-Conditions.aspx",
+    "LinkPrivacy": "Privacy policy",
     "URLPrivacy": "https://gcxgce.sharepoint.com/SitePages/Terms-and-Conditions.aspx#privacy-statement",
+    "LinkAccessibility": "Accessibility",
+    "URLAccessibility": "https://gcxgce.sharepoint.com/sites/Accessibility/SitePages/Home.aspx",
+
+    //Second Column
+    "ExploreColumnTitle": "EXPLORE",
+    "LinkCommunities": "Communities",
+    "URLCommunities": "https://gcxgce.sharepoint.com/sites/Communities/SitePages/Home.aspx",
+    "LinkCareerMarketplace": "Career Marketplace",
+    "URLCareerMarketplace": "https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/Find-an-Opportunity---Career-Marketplace.aspx",
+
+    //Third Column  
+    "SupportColumnTitle": "SUPPORT",
+    "LinkSupport": "Support hub",
     "URLSupport": "https://gcxgce.sharepoint.com/sites/Support",
+    "LinkGCXchangeDirectory":"GCXchange directory hub",
+    "URLGCXchangeDirectory": "https://gcxgce.sharepoint.com/SitePages/GCXchangeDirectory.aspx",
+    "FAQLink": "Frequently asked questions",
+    "URLFAQ": "https://gcxgce.sharepoint.com/sites/Support/SitePages/FAQ.aspx",
+    "AskMeAnythingLink": "Ask me anything",
+    "URLAskMeAnything": "https://gcxgce.sharepoint.com/sites/Support/_layouts/15/Events.aspx?Page=%2Fsites%2FSupport%2FSitePages%2FHome.aspx&InstanceId=44a77339-43da-4321-93fc-38de644bdf50&AudienceTarget=false",
+
+    //Fourth Column
+    "ResourcesColumnTitle": "RESOURCES",
     "URLIntranet": "https://intranet.canada.ca/index-eng.asp",
+    "URLPedia": "https://www.gcpedia.gc.ca/wiki/Main_Page",
     "URLCampus": "https://www.csps-efpc.gc.ca/GCcampus/index-eng.aspx",
     "LabelCampus":"Learning platform",
-    "URLPedia": "https://www.gcpedia.gc.ca/wiki/Main_Page",
-    "URLGCXchangeDirectory": "https://gcxgce.sharepoint.com/SitePages/GCXchangeDirectory.aspx",
-    "LinkGCXchangeDirectory":"GCXchange Directory",
     "GovImage":"Government of Canada",
     "CanadaImage":"Symbol of the Government of Canada",
-    "ProtectedTermsURL": "https://gcxgce.sharepoint.com/SitePages/Terms-and-Conditions-pro-b.aspx",
-    "ProtectedTerms": "Protected A/B Terms and Conditions",
-    "LinkAccessibility": "Accessibility",
-    "URLAccessibility": "https://gcxgce.sharepoint.com/sites/Accessibility/SitePages/Home.aspx"
+    // "ProtectedTermsURL": "https://gcxgce.sharepoint.com/SitePages/Terms-and-Conditions-pro-b.aspx",
+    // "ProtectedTerms": "Protected A/B Terms and Conditions",
   }
 });
--- src/webparts/footerWebpart/loc/fr-fr.js
+++ src/webparts/footerWebpart/loc/fr-fr.js
@@ -4,28 +4,47 @@ define([], function() {
     "PropertyPaneDescription": "Description",
     "BasicGroupName": "Group Name",
     "DescriptionFieldLabel": "Description Field",
-    "AboutUsTitle": "NOTRE ÉQUIPE",
-    "SiteLinksTitle": "VOIR LE RESTE DU SITE",
-    "SupportLinksTitle": "COMMUNIQUEZ AVEC VOS COLLÈGUES",
+
+    //First Column
+    "GCXchangeColumnTitle": "GCÉCHANGE",
     "LinkAboutUs": "À propos de nous",
-    "LinkTerms": "Les conditions d' utilisation de GCÉchange",
+    "LinkTerms": "Conditions d' utilisation",
     "LinkPrivacy": "Politique de confidentialité",
-    "LinkSupport": "Soutien",
     "URLAboutUs": "https://gcxgce.sharepoint.com/SitePages/fr/About-us.aspx",
     "URLTerms": "https://gcxgce.sharepoint.com/SitePages/fr/Terms-and-Conditions.aspx",
     "URLPrivacy": "https://gcxgce.sharepoint.com/SitePages/fr/Terms-and-Conditions.aspx#%C3%A9nonc%C3%A9-de-confidentialit%C3%A9",
+    "LinkAccessibility": "Accessibilité",
+    "URLAccessibility": "https://gcxgce.sharepoint.com/sites/Accessibility/SitePages/fr/Home.aspx",
+
+    //Second Column
+    "ExploreColumnTitle": "EXPLORER",
+    "LinkCommunities": "Collectivités",
+    "URLCommunities": "https://gcxgce.sharepoint.com/sites/Communities/SitePages/fr/Home.aspx",
+    "LinkCareerMarketplace": "Marché de carrière",
+    "URLCareerMarketplace": "https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/fr/Find-an-Opportunity---Career-Marketplace.aspx",
+
+    //Third Column
+    "SupportColumnTitle": "SOUTIEN",
+    "LinkSupport": "Centre de soutien",
     "URLSupport": "https://gcxgce.sharepoint.com/sites/Support/SitePages/fr/Home.aspx",
+    "FAQLink": "Foires aux questions",
+    "URLFAQ": "https://gcxgce.sharepoint.com/SitePages/fr/FAQ.aspx",
+    "AskMeAnythingLink": "Demandez-moi n’importe quoi",
+    "URLAskMeAnything": "https://gcxgce.sharepoint.com/sites/Support/_layouts/15/Events.aspx?Page=%2Fsites%2FSupport%2FSitePages%2Ffr%2FHome.aspx&InstanceId=a9ef6f15-e041-412e-9013-b891ab80e25f&AudienceTarget=false",
+    "LinkGCXchangeDirectory":"Annuaire GCÉchange",
+    "URLGCXchangeDirectory": "https://gcxgce.sharepoint.com/SitePages/fr/GCXchangeDirectory.aspx",
+
+    //Fourth Column
+    "ResourcesColumnTitle": "RESSOURCES",
     "URLIntranet": "https://intranet.canada.ca/index-fra.asp",
     "URLCampus": "https://www.csps-efpc.gc.ca/gccampus/index-fra.aspx",
     "LabelCampus":"Plateforme d'apprentissage",
     "URLPedia": "https://www.gcpedia.gc.ca/wiki/Main_Page?setlang=fr&uselang=fr",
-    "URLGCXchangeDirectory": "https://gcxgce.sharepoint.com/SitePages/fr/GCXchangeDirectory.aspx",
-    "LinkGCXchangeDirectory":"Annuaire GCÉchange",
+   
+  
     "GovImage":"Gouvernement du Canada",
     "CanadaImage":"Symbole du gouvernement du Canada",
-    "ProtectedTermsURL": "https://gcxgce.sharepoint.com/SitePages/fr/Terms-and-Conditions-pro-b.aspx",
-    "ProtectedTerms": "Les conditions d'utilisation de Pro A/B",
-    "LinkAccessibility": "Accessibilité",
-    "URLAccessibility": "https://gcxgce.sharepoint.com/sites/Accessibility/SitePages/fr/Home.aspx"
+    // "ProtectedTermsURL": "https://gcxgce.sharepoint.com/SitePages/fr/Terms-and-Conditions-pro-b.aspx",
+    // "ProtectedTerms": "Les conditions d'utilisation de Pro A/B",
   }
 });
